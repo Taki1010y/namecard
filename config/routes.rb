3e984bcb58d0ed758e31d610068ef893bdd77a75
Taki1010y <takizawa.1211rou@mail.com>
2022-04-12 03:30:57 +0900

    遷移先の設定 wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root :to => "web#index"
+  root :to => "users#sessions"
+  
 
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
