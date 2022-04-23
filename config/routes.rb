b9a55e0b5b1ae4a8cccbc03ff920b789463927f8
Taki1010y <takizawa.1211rou@mail.com>
2022-04-24 04:18:47 +0900

    cloud9のurl以外着手

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,7 @@
 Rails.application.routes.draw do
   root :to => "web#index"
+  get 'detail' => "detail#index"
+  get 'register' => "register#index"
   
   devise_for :users, :controllers => {
     :registrations => 'users/registrations',
