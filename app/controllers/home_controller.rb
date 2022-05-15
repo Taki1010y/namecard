b19db76f6ac8f814eb37e87f3408aec1c089a716
Taki1010y <takizawa.1211rou@mail.com>
2022-05-15 11:15:27 +0900

    wip

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -35,7 +35,7 @@ end
 
 private
   def home_params
-    params.require(:home).permit(:name, :school, :address, :phone, :department, :favorite_a, :favorite_b, :favorite_c, :favorite_d, :image, :image_first, portfolio_attributes: [:id, :title, :caption, :image])
+    params.require(:home).permit(:name, :school, :address, :phone, :department, :favorite_a, :favorite_b, :favorite_c, :favorite_d, :image, portfolio_attributes: [:id, :title, :caption, :image])
   end
 
   def set_home
--- app/models/portfolio.rb
+++ app/models/portfolio.rb
@@ -1,3 +1,4 @@
 class Portfolio < ApplicationRecord
     belongs_to :home
+    mount_uploader :image, ImageUploaders
 end
