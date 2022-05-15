565de0be997d02046431f6dc6984e02e7ad80b68
Taki1010y <takizawa.1211rou@mail.com>
2022-05-15 14:22:25 +0900

    wip

--- app/controllers/company_infos_controller.rb
+++ app/controllers/company_infos_controller.rb
@@ -25,6 +25,20 @@ def create
     end
 end
 
+def edit
+    @company_info = CompanyInfo.find_by_id(params[:id])
+end
+
+def update
+    @company_info = CompanyInfo.find(params[:id])
+    
+    if @company_info.update(company_info_params)
+        redirect_to @company_info
+    else
+        render 'edit'
+    end
+end
+
 def favorite
     current_user.toggle_like!(@company_info)
     redirect_to company_info_url @company_info
